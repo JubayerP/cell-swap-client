44fb03a6c4c38eaa0eec9c5ff2d8521258fc03a1
JubayerDev <ajashes858@gmail.com>
2023-01-05 01:55:26 +0600

    slider added responsive

---
+++ src/Pages/Slider/Slider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import slider1 from '../../assets/slider/slider-1.jpeg'
+import slider2 from '../../assets/slider/slider-2.jpeg'
+import slider3 from '../../assets/slider/slider-3.jpeg'
+
+const Slider = () => {
+    return (
+        <div>
+            <AwesomeSlider>
+                <div data-src={slider1}>
+                    
+                </div>
+                <div data-src={slider2}>2</div>
+                <div data-src={slider3}>3</div>
+            </AwesomeSlider>
+        </div>
+    );
+};
+
+export default Slider;
