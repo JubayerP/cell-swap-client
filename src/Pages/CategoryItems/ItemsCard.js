66ee8d866b28544722012d5601a219bb08363b05
JubayerDev <ajashes858@gmail.com>
2023-01-08 02:37:34 +0600

    wishlist feature added

--- src/Pages/CategoryItems/ItemsCard.js
+++ src/Pages/CategoryItems/ItemsCard.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
 import { FaCartArrowDown } from 'react-icons/fa';
 import { MdOutlineReportGmailerrorred } from 'react-icons/md';
 import { TbJewishStar } from 'react-icons/tb';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const ItemsCard = ({ phone, openModal, setBookingProduct }) => {
     const { name, image, location, resalePrice, originalPrice, used, date, sellerName } = phone;
+    const { user } = useContext(AuthContext);
+
+
+
+    const handleWishlist = phone => {
+        fetch(`http://localhost:5000/wishlist?id=${phone._id}`,{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({email: user?.email})
+        })
+            .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            toast.success('Added to your wishlist!')
+        })
+    }
 
     return (
         <div className='mx-auto p-6 max-w-sm w-full rounded-xl shadow-xl'>
@@ -24,7 +44,7 @@ const ItemsCard = ({ phone, openModal, setBookingProduct }) => {
 
                 <div className='space-y-3'>
                     <button onClick={() => { openModal(); setBookingProduct(phone) }} className='bg-black px-4 py-2 text-white rounded cursor-pointer flex justify-center items-center space-x-2 w-full'><span>Book Now</span> <FaCartArrowDown /></button>
-                    <button className='bg-yellow-700 px-4 py-2 text-white rounded cursor-pointer flex justify-center items-center space-x-2 w-full'><span>Add To Wishlist</span> < TbJewishStar/></button>
+                    <button onClick={()=>handleWishlist(phone)} className='bg-yellow-700 px-4 py-2 text-white rounded cursor-pointer flex justify-center items-center space-x-2 w-full'><span>Add To Wishlist</span> < TbJewishStar/></button>
                     <button className='bg-red-600 px-4 py-2 text-white rounded cursor-pointer flex justify-center items-center space-x-2 w-full'><span>Report To Admin</span> <MdOutlineReportGmailerrorred /></button>
                 </div>
             </div>
