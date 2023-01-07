09c011e0ec738f782711bb20b28841bdc5457bd4
JubayerDev <ajashes858@gmail.com>
2023-01-07 22:31:20 +0600

    footer added

--- src/Pages/Dashboard/DashboardSidebar/DashboardSidebar.js
+++ src/Pages/Dashboard/DashboardSidebar/DashboardSidebar.js
@@ -15,9 +15,9 @@ const DashboardSidebar = () => {
         <div className="relative min-h-screen md:flex" data-dev-hint="container">
             <input type="checkbox" id="menu-open" className="hidden" />
 
-            <label for="menu-open" className="absolute right-2 bottom-2 shadow-lg rounded-full p-2 bg-gray-100 text-gray-600 md:hidden" data-dev-hint="floating action button">
+            <label htmlFor="menu-open" className="absolute right-2 bottom-2 shadow-lg rounded-full p-2 bg-gray-100 text-gray-600 md:hidden" data-dev-hint="floating action button">
                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </label>
 
@@ -26,18 +26,18 @@ const DashboardSidebar = () => {
                     Your App is cool
                 </a>
 
-                <label for="menu-open" id="mobile-menu-button" className="m-2 p-2 focus:outline-none hover:text-white hover:bg-gray-700 rounded-md">
+                <label htmlFor="menu-open" id="mobile-menu-button" className="m-2 p-2 focus:outline-none hover:text-white hover:bg-gray-700 rounded-md">
                     <svg id="menu-open-icon" className="h-6 w-6 transition duration-200 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                     <svg id="menu-close-icon" className="h-6 w-6 transition duration-200 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </label>
             </header>
 
-            <aside id="sidebar" className="text-gray-800 bg-white md:w-64 w-44 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto" data-dev-hint="sidebar; px-0 for frameless; px-2 for visually inset the navigation">
-                <div className="flex flex-col space-y-6" data-dev-hint="optional div for having an extra footer navigation">
+            <aside id="sidebar" className="text-gray-800 bg-white md:w-64 w-44 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transhtmlForm md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto" data-dev-hint="sidebar; px-0 htmlFor frameless; px-2 htmlFor visually inset the navigation">
+                <div className="flex flex-col space-y-6" data-dev-hint="optional div htmlFor having an extra footer navigation">
                     {isSeller && <>
                         <Link to='/dashboard/addproduct' className='pl-10 font-semibold hover:bg-gray-100 py-2 rounded-xl'>Add A Product</Link>
                         <Link to='/dashboard/myproducts' className='pl-10 font-semibold hover:bg-gray-100 py-2 rounded-xl'>My Products</Link>
--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -14,10 +14,10 @@ const Categories = () => {
     return (
         <div className='my-28 mx-auto container'>
             <h2 className="text-4xl font-bold text-center mb-6">Mobile Phones Categories</h2>
-            <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 space-y-3'>
                 {
                     categories.map(c =>
-                        <div key={c._id} className='mx-auto p-2 rounded-md cursor-pointer shadow-md border border-black'>
+                        <div key={c._id} className='mx-auto p-8 rounded-md cursor-pointer shadow-md border-black'>
                             <Link to={`/categories/${c.category}`}>
                                 <img className='w-28 rounded-md mx-auto mb-2' src={c.categoryImage} alt="" />
                                 <h3 className='text-xl font-semibold '>{c.categoryName}</h3>
